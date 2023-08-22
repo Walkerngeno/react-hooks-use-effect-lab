a9eb6e9578ed3592f642ebce41b6ae8ef9c17769
KIPTOYOT BRIAN <ngenobrayo7@gmail.com>
2023-08-22 21:20:45 +0300

    commited assignment

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,13 @@ import quiz from "../data/quiz";
 function App() {
   const [questions, setQuestions] = useState(quiz);
   const [currentQuestionId, setCurrentQuestion] = useState(1);
-  const [score, setScore] = useState(0);
   const currentQuestion = questions.find((q) => q.id === currentQuestionId);
 
-  function handleQuestionAnswered(correct) {
+  function handleQuestionAnswered() {
     if (currentQuestionId < questions.length) {
-      setCurrentQuestion((currentQuestionId) => currentQuestionId + 1);
+      setCurrentQuestion(currentQuestionId + 1);
     } else {
       setCurrentQuestion(null);
-    }
-    if (correct) {
-      setScore((score) => score + 1);
     }
   }
 
@@ -28,10 +24,7 @@ function App() {
             onAnswered={handleQuestionAnswered}
           />
         ) : (
-          <>
-            <h1>Game Over</h1>
-            <h2>Total Correct: {score}</h2>
-          </>
+          <h1>Game Over</h1>
         )}
       </section>
     </main>
